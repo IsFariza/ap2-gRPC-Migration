a93626bef0559433793b3501a61ee8dcd14e6df4
IsFariza <fariza.ismag@gmail.com>
2026-04-14 02:18:55 +0500

    feat: add repo and usecase interfaces

---
+++ doctor-service/internal/model/interfaces/doctor_usecase.go
@@ -0,0 +1,13 @@
+package interfaces
+
+import (
+	"context"
+
+	"github.com/IsFariza/ap2-gRPC-Migration/doctor-service/internal/model"
+)
+
+type DoctorUseCase interface {
+	Create(ctx context.Context, doc model.Doctor) (*model.Doctor, error)
+	GetByID(ctx context.Context, id string) (*model.Doctor, error)
+	List(ctx context.Context) ([]*model.Doctor, error)
+}
