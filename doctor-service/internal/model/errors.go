80a8f31bb1ab7b034884ee890b86ebd8a59c021b
IsFariza <fariza.ismag@gmail.com>
2026-04-14 05:43:14 +0500

    remove general go.mod files

--- doctor-service/internal/model/errors.go
+++ doctor-service/internal/model/errors.go
@@ -3,10 +3,10 @@ package model
 import "errors"
 
 var (
-	ErrNameRequired           = errors.New("doctor name is required")
-	ErrEmailRequired          = errors.New("doctor email is required")
-	ErrInvalidEmail           = errors.New("invalid email format")
-	ErrInvalidID              = errors.New("invalid doctor ID format")
+	ErrNameRequired  = errors.New("doctor name is required")
+	ErrEmailRequired = errors.New("doctor email is required")
+	ErrInvalidEmail  = errors.New("invalid email format")
+	ErrInvalidID     = errors.New("invalid doctor ID format")
 
 	ErrEmailUsed = errors.New("a doctor with this email already exists")
 
